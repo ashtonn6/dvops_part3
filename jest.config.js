31ffbc44b0fa1196fb5877390a0bedf385acc892
ashtonn6 <2400769b@student.tp.edu.sg>
2026-02-09 03:27:43 +0630

    Update testMatch pattern in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: [
-    '**/__tests__/**/*.test.js',
+    '**/tests/**/*.test.js',
     '**/?(*.)+(spec|test).js'
   ],
   testPathIgnorePatterns: [
