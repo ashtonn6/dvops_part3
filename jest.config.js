6718c3e0ad6a3bf508b733c5df366e82a4d21f71
ashtonn6 <2400769b@student.tp.edu.sg>
2026-02-09 03:27:18 +0630

    Add Jest configuration file for testing setup

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.test.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '**/*.playwright.test.js'
+  ],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!**/node_modules/**'
+  ],
+  verbose: true
+};
